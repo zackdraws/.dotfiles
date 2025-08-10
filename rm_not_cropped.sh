b6aaa353189639523e3932c068ea5ad00dce2ef4
zackdraws <zacharyjohnson@lcad.edu>
2025-08-10 13:47:13 -0700

    remove if doesn't include word fish script

---
+++ rm_not_cropped.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Searching for files to delete (excluding files with 'cropped' in the name)..."
+
+find . -type f ! -name '*cropped*' -print
+
+read -p "Are you sure you want to delete these files? (y/N): " confirm
+
+if [[ "$confirm" == [yY] ]]; then
+
+    find . -type f ! -name '*cropped*' -delete
+
+    echo "Files deleted."
+
+else
+
+    echo "Operation cancelled."
+
+fi
