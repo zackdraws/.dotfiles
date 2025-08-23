ddba0aa1e6325f839d90e45ba0960d17c743f762
zackdraws <zacharyjohnson@lcad.edu>
2025-08-23 12:14:52 -0700

    update

--- fish/functions/fish_mode_prompt.fish
+++ fish/functions/fish_mode_prompt.fish
@@ -1,42 +1,49 @@
-# Display the current binding mode... if it's vi or vi-like.
-#
-# To always show the binding mode (regardless of current bindings):
-#     set -g theme_display_vi yes
-#
-# To never show:
-#     set -g theme_display_vi no
+function fish_mode_prompt -d 'Display vi mode in prompt if using vi-like bindings'
+    if test "$theme_display_vi" != "no"
+        if test "$fish_key_bindings" = "fish_vi_key_bindings"
+            or test "$fish_key_bindings" = "hybrid_bindings"
+            or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
+            or test "$theme_display_vi" = "yes"
 
-function fish_mode_prompt -d 'bobthefish-optimized fish mode indicator'
-    [ "$theme_display_vi" != 'no' ]
-    or return
+            # Fallbacks
+            set -q color_vi_mode_default; or set color_vi_mode_default blue
+            set -q color_vi_mode_insert; or set color_vi_mode_insert green
+            set -q color_vi_mode_visual; or set color_vi_mode_visual magenta
 
-    [ "$fish_key_bindings" = 'fish_vi_key_bindings' \
-        -o "$fish_key_bindings" = 'hybrid_bindings' \
-        -o "$fish_key_bindings" = 'fish_hybrid_key_bindings' \
-        -o "$theme_display_vi" = 'yes' ]
-    or return
+            # Clean any bad quotes or characters from values
+            function __clean_color --argument color
+                set cleaned (string replace -r "[\"']" '' $color)
+                if string match -rq '^#?[0-9a-fA-F]{6}$' -- $cleaned
+                    echo $cleaned
+                else if string match -rq '^[a-zA-Z]+$' -- $cleaned
+                    echo $cleaned
+                else
+                    echo normal
+                end
+            end
 
-    __bobthefish_colors $theme_color_scheme
+            set default_color (__clean_color $color_vi_mode_default)
+            set insert_color (__clean_color $color_vi_mode_insert)
+            set visual_color (__clean_color $color_vi_mode_visual)
 
-    type -q bobthefish_colors
-    and bobthefish_colors
+            set_color normal
 
-    set_color normal # clear out anything bold or underline...
+            switch $fish_bind_mode
+                case default
+                    set_color -b $default_color
+                    echo -n ' N '
+                case insert
+                    set_color -b $insert_color
+                    echo -n ' I '
+                case replace replace_one replace-one
+                    set_color -b $insert_color
+                    echo -n ' R '
+                case visual
+                    set_color -b $visual_color
+                    echo -n ' V '
+            end
 
-    switch $fish_bind_mode
-        case default
-            set_color -b $color_vi_mode_default
-            echo -n ' N '
-        case insert
-            set_color -b $color_vi_mode_insert
-            echo -n ' I '
-        case replace replace_one replace-one
-            set_color -b $color_vi_mode_insert
-            echo -n ' R '
-        case visual
-            set_color -b $color_vi_mode_visual
-            echo -n ' V '
+            set_color normal
+        end
     end
-
-    set_color normal
 end
