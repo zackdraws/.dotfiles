aec279bd13fdc5bb108f3ea6a6c21f2cb9c08bd4
zackdraws <zacharyjohnson@lcad.edu>
2025-08-23 12:14:38 -0700

    fixed error

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,5 @@
 set -gx EDITOR emacs -nw
-set -gx PATH $PATH /home/zack/.local/bin
-set -Ux PATH $PATH /home/zack/.local/bin
 alias e=$EDITORS
-alias record="ffmpeg.exe -y -f gdigrab -framerate 30 -i desktop "$(wslpath =w $HOME)/zpf3.mp4"
-alias copydir="xsel -b"
 alias se="sudo $EDITOR"
 alias ghostty='cd ~/Projects/ghostty && ./zig-out/bin/ghostty &'
 alias gh="zig-out/bin/ghostty"
@@ -19,10 +15,10 @@ alias copy_txt="copy_clip.sh"
 alias compress_v="compress_video.sh"
 alias sync="syncthing.sh"
 alias ok="TODO.sh"
-alias todo="mkto.sh"
+alias dotodo="mkto.sh"
 alias yt="mov-cli -s youtube.yt-dlp"
 alias ytd="mov-cli -s youtube.yt-dlp -d"
-
+alias ze="zellij"
 zoxide init fish | source
 set -gx GDK_SCALE 2 #GWSL
 
