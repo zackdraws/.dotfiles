f3f64e908f73678dac610abb0393c425756d97f1
zackdraws <zacharyjohnson@lcad.edu>
2025-09-04 05:16:41 -0700

    update(git ignore)

--- ok.sh
+++ ok.sh
@@ -46,3 +46,30 @@ brew install tuple
 brew install zed
 
 brew cleanup
+
+# Create directory if it doesn't exist
+[ -d ~/.config ] || mkdir ~/.config
+
+ln -s ~/.dotfiles/config/fish ~/.config/
+ln -s ~/.dotfiles/config/kitty ~/.config/
+ln -s ~/.dotfiles/lazygit ~/.config/
+ln -s ~/.dotfiles/yazi ~/.config/
+ln -s ~/.dotfiles/nvim ~/.config/
+ln -s ~/.dotfiles/ghostty ~/.config/
+
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.hushlogin ~/.hushlogin
+
+
+# Create directory if it doesn't exist
+[ -d ~/.config ] || mkdir ~/.config
+
+ln -s ~/.dotfiles/config/fish ~/.config/
+ln -s ~/.dotfiles/config/kitty ~/.config/
+ln -s ~/.dotfiles/lazygit ~/.config/
+ln -s ~/.dotfiles/yazi ~/.config/
+ln -s ~/.dotfiles/nvim ~/.config/
+ln -s ~/.dotfiles/ghostty ~/.config/
+
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.hushlogin ~/.hushlogin
