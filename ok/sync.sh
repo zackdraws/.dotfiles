2fe231065f85e8d7f1020ee058bd5324bd2ff5de
zackdraws <zacharyjohnson@lcad.edu>
2025-09-13 17:11:48 -0700

    update

---
+++ ok/sync.sh
@@ -0,0 +1,9 @@
+sudo ln -s ~/.dotfiles/fish.ps1/ /mnt/c/users/zacha/documents/windowsPowershell/Scripts/
+sudo ln -s  ~/.dotfiles/sh/ /usr/local/bin 
+sudo ln -s ~/.dotfiles/py/
+sudo ln -s ~/.dotfiles
+sudo ln -s ~/.dotfiles/emacs ~/.emacs
+sudo ln -s ~/.dotfiles/ghostty ~/config
+sudo ln -s ~/.dotfiles/marp
+sudo ln -s ~/.dotfiles/ps
+sudo ln -s ~/.dotfiles/fish
