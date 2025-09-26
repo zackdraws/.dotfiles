54cf7a03065e64fafc65dfe25033e608247ea654
zackdraws <zacharyjohnson@lcad.edu>
2025-09-25 22:27:01 -0700

    tidied up convert shells scripts

--- sh/convert/cnv_m4a_mp3.sh
+++ sh/convert/cnv_m4a_mp3.sh
@@ -1,31 +1,21 @@
 #!/usr/bin/env bash
-
 set -e
-
 echo "Converting all .m4a files in: $(pwd)"
-
 echo "Preserving metadata"
-
 shopt -s nullglob
 shopt -s nocaseglob
-
 for AUDIO_FILE in *.m4a; do
   if [[ -n "$AUDIO_FILE" ]]; then  # Check if the file exists (handles nullglob)
     OUTPUT_FILE="${AUDIO_FILE%.m4a}.mp3"
-
     echo "Converting: \"$AUDIO_FILE\" ΓåÆ \"$OUTPUT_FILE\""
-
     ffmpeg -y -i "$AUDIO_FILE" \
         -map_metadata 0 \
         -codec:a libmp3lame \
         -b:a 192k \
         "$OUTPUT_FILE"
-
     echo "Done: \"$OUTPUT_FILE\""
-
     echo
   fi
 done
-
 echo "All .m4a files converted to .mp3 with metadata preserved!"
 ~
