b905893cdfe5ab8c3c0d0f17c5e717826873d5db
zackdraws <zacharyjohnson@lcad.edu>
2025-11-19 22:00:44 -0800

    made seperate file for installing programs through scoop

---
+++ ok/ok-scoop.sh
@@ -0,0 +1,8 @@
+scoop install git 7zip
+scoop bucket add extras
+scoop bucket add versions
+scoop bucket add nerd-fonts
+scoop install fzf gh yazi fish syncthing neovim
+scoop install blender-lts
+scoop install discord
+scoop install nerd-fonts/FiraCode-NF
