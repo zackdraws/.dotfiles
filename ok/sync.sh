44c90e56422ddb04fe73511070a58e35c8ff1c21
zack1217 <zacharyjohnson@lcad.edu>
2025-12-21 11:17:10 -0800

    took out stuff that doesn't work

--- ok/sync.sh
+++ ok/sync.sh
@@ -1,8 +1,4 @@
-sudo ln -s ~/.dotfiles/fish.ps1/ /mnt/c/users/zacha/documents/windowsPowershell/Scripts/
-sudo ln -s ~/.dotfiles/py/
-sudo ln -s ~/.dotfiles
 sudo ln -s ~/.dotfiles/emacs ~/.emacs
 sudo ln -s ~/.dotfiles/ghostty ~/config
-sudo ln -s ~/.dotfiles/marp
-sudo ln -s ~/.dotfiles/ps
-sudo ln -s ~/.dotfiles/fish
+sudo ln -s ~/.dotfiles/marp ~/marp
+sudo ln -s ~/.dotfiles/ps ~/ps
