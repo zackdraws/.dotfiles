4eeeefe327884f73d50fdcd4e3fbc7746910ca71
zack1217 <zacharyjohnson@lcad.edu>
2025-12-21 11:14:31 -0800

    took out sh folder

--- ok/sync.sh
+++ ok/sync.sh
@@ -1,5 +1,4 @@
 sudo ln -s ~/.dotfiles/fish.ps1/ /mnt/c/users/zacha/documents/windowsPowershell/Scripts/
-sudo ln -s  ~/.dotfiles/sh/ /usr/local/bin 
 sudo ln -s ~/.dotfiles/py/
 sudo ln -s ~/.dotfiles
 sudo ln -s ~/.dotfiles/emacs ~/.emacs
