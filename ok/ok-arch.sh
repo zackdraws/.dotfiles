1d8cae4408ba2f0769528b65f9212cddda20de68
zack1217 <zacharyjohnson@lcad.edu>
2026-03-29 22:08:34 -0700

    update

--- ok/ok-arch.sh
+++ ok/ok-arch.sh
@@ -1,14 +1,14 @@
-;; set time
+# set time
 sudo timedatectl set-timezone America/Los_Angeles
-;; install
+# install
 pacman -S git curl emacs discord syncthing tmux wofi wlroots xdg-desktop-portal-hyprland ncdu xbindkeys xdotool keyd
 yay -S interception-tools interception-keyboard
 sudo systemctl enable --now keyd
-;;set-up emacs
+#set-up emacs
 cp ~/.dotfiles/emacs/.emacs ~/.emacs
 mkdir ~/.ok/ok/
 chsh -s $(which fish)
-;; fonts and pdf
+# fonts and pdf
 sudo pacman -S xelatex
 sudo pacman -S ttf-dejavu
 mkdir /usr/share/fonts/truetype/dejavus 
@@ -16,18 +16,20 @@ ln -s /usr/share/fonts/TTF/DejaVuSans.ttf /usr/share/fonts/truetype/dejavu/DejaV
 pacman -S python-pillow
 pacman -S pandoc imagemagick
 yay -S sioyek python-fpdf 
-;; finance
+# finance
 sudo pacman -S hledger
 pipx install hledger-utils
 yay -S pup
-;; monitor system
+# monitor system
 pacman -S htop btop
-;; tmux tpm plug-ins
+# tmux tpm plug-ins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-;;dvd-
+#dvd-
 sudo pacman -S libdvdnav
 sudo pacman -S libdvdcss
 sudo pacman -S libdvdread
-;;cmd to play dvd
-;;mpv dvd://
+#cmd to play dvd
+#mpv dvd://
 paru -S google-chrome
+sudo apt install build-essential pkg-config libgmime-3.0-dev libxapian-dev libsqlite3-dev   libgpgme-dev libglib2.0-dev
+
