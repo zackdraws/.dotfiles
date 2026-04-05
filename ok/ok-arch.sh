a75027f1d161a04c468891a83845451510345c5c
zack1217 <zacharyjohnson@lcad.edu>
2026-04-04 19:24:53 -0700

    update

--- ok/ok-arch.sh
+++ ok/ok-arch.sh
@@ -32,3 +32,4 @@ sudo pacman -S libdvdread
 # mpv dvd://
 paru -S google-chrome
 sudo apt install build-essential pkg-config libgmime-3.0-dev libxapian-dev libsqlite3-dev libgpgme-dev libglib2.0-dev
+pacman -S imv
