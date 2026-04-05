98f241f199c8fa768a153e11ee74a81f6efd9a69
zack1217 <zacharyjohnson@lcad.edu>
2026-04-05 15:52:31 -0700

    script for screen recording

---
+++ sh/run/record.sh
@@ -0,0 +1,22 @@
+k#!/usr/bin/env bash
+
+OUT="$HOME/Videos/rec_$(date +%Y-%m-%d_%H-%M-%S).mp4"
+PIDFILE="/tmp/hypr-record.pid"
+
+start() {
+  wf-recorder -f "$OUT" &
+  echo $! > "$PIDFILE"
+  notify-send "Recording started"
+}
+
+stop() {
+  kill -INT "$(cat "$PIDFILE")"
+  rm -f "$PIDFILE"
+  notify-send "Recording stopped"
+}
+
+if [[ -f "$PIDFILE" ]]; then
+  stop
+else
+  start
+fi
