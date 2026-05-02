9953c6643629a28d4edd7ec97ecc29b703ae0a46
zack1217 <zacharyjohnson@lcad.edu>
2026-05-02 07:58:33 -0700

    update

--- ok/ok-pc.sh
+++ ok/ok-pc.sh
@@ -6,8 +6,8 @@ echo "link from .dotfiles to emacs"
 ln -s ~/.dotfiles/emacs/.emacs ~/.emacs.d/init.el
 echo "download oh-my-posh and fonts"
 pacman -S mingw-w64-ucrt-x86_64-oh-my-posh mingw-w64-ucrt-x86_64-yazi mingw-w64-ucrt-x86_64-ttf-firacode-nerd
+cd ~/
 git clone https://github.com/JanDeDobbeleer/oh-my-posh.git
-echo "installed needed programs"
 pacman -Syu --noconfirm
 pacman -S --noconfirm mpv
 pacman -S --needed base-devel git fzf fish gh yazi syncthing wget curl unzip zip tar neovim htop ripgrep fd bat jq tree tmux zoxide lazygit glow xclip bat ripgrep fd
@@ -15,7 +15,6 @@ echo "github cli, python, toolchain, chromaprint, ffmpeg, libffi, and libyaml"
 pacman -S mingw-w64-ucrt-x86_64-github-cli mingw-w64-ucrt-x86_64-python
 pacman -S --needed base-devel mingw-w64-ucrt-x86_64-toolchain
 pacman -S --needed mingw-w64-ucrt-x86_64-chromaprint mingw-w64-ucrt-x86_64-ffmpeg mingw-w64-ucrt-x86_64-libffi mingw-w64-ucrt-x86_64-libyaml mingw-w64-ucrt-x86_64-mpv mingw-w64-ucrt-x86_64-fzf  mingw-w64-ucrt-x86_64-vlc mingw-w64-ucrt-x86_64-github-cli irssi #
-
 echo "install beets, tex"
 pip install 'beets[fetchart,lyrics,lastgenre,ftintitle,chromaprint]'
 pacman -S mingw-w64-ucrt-x86_64-texlive-bin \
@@ -25,24 +24,6 @@ pacman -S mingw-w64-ucrt-x86_64-texlive-bin \
         mingw-w64-ucrt-x86_64-texlive-latex-extra
 pacman -S mingw-w64-ucrt-x86_64-gcc \
              mingw-w64-ucrt-x86_64-libevent mingw-w64-ucrt-x86_64-ncurses \
-# foobar2000 installation
-DOWNLOADS_DIR="$HOME/Downloads"
-mkdir -p "$DOWNLOADS_DIR"
-cd "$DOWNLOADS_DIR"
-echo "==> Downloading foobar2000 installer..."
-curl -LO https://www.foobar2000.org/files/foobar2000_v2.1.exe
-echo "==> Launching foobar2000 installer..."
-cmd.exe /C start foobar2000_v2.1.exe
-# Zen Browser installation
-ZEN_BROWSER_URL="https://example.com/zenbrowser_installer.exe"  # Replace with actual URL if known
-echo "==> Downloading Zen Browser (if available)..."
-if curl --output zenbrowser_installer.exe --location --fail "$ZEN_BROWSER_URL"; then
-    echo "==> Launching Zen Browser installer..."
-    cmd.exe /C start zenbrowser_installer.exe
-else
-    echo "==> Zen Browser URL not valid or not provided."
-    echo "    Please update the script with a valid Zen Browser download link."
-fi
 # Add fish to /etc/shells if it's not there
 command -v fish | sudo tee -a /etc/shells
 echo  "Change default shell to fish"
@@ -58,3 +39,7 @@ winget vlc
 # scoop
 * scoop bucket add extreas
 - scoop install extras/mpv
+# controller driver from sony
+curl -L "https://fwupdater.dl.playstation.net/fwupdater/PlayStationAccessoriesInstaller.exe" ^
+-o "C://users/ok/Downloads/"
+winget install AppWork.JDownloader
