0c426fccbff9ddc0217d4afc52cbee783a9e32f7
zack1217 <zacharyjohnson@lcad.edu>
2026-09-24 01:52:40 -0700

    update

--- ok/ok-arch.sh
+++ ok/ok-arch.sh
@@ -1,35 +1,19 @@
 # set time
 sudo timedatectl set-timezone America/Los_Angeles
-# install
-pacman -S git curl emacs discord syncthing tmux wofi wlroots xdg-desktop-portal-hyprland ncdu xbindkeys xdotool keyd
-yay -S interception-tools interception-keyboard
-sudo systemctl enable --now keyd
+# install programs
+sudo chmod +X ./~/.dotfiles/sh/install/install-arch-software
+sudo ln -s ~/.dotfiles/sh/install/install-arch-software /usr/local/bin/
+./home/user/ok/.dotfiles/sh/install/install-arch-software
 #set-up emacs
 cp ~/.dotfiles/emacs/.emacs ~/.emacs
 mkdir ~/.ok/ok/
+# fish
 chsh -s $(which fish)
 # fonts and pdf
-sudo pacman -S xelatex
-sudo pacman -S ttf-dejavu
 mkdir /usr/share/fonts/truetype/dejavus 
 ln -s /usr/share/fonts/TTF/DejaVuSans.ttf /usr/share/fonts/truetype/dejavu/DejaVuSans.ttf
-pacman -S python-pillow
-pacman -S pandoc imagemagick
-yay -S sioyek python-fpdf 
-# finance
-sudo pacman -S hledger
-pipx install hledger-utils
-yay -S pup
-# monitor system
-pacman -S htop btop
 # tmux tpm plug-ins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 #dvd-
-sudo pacman -S libdvdnav
-sudo pacman -S libdvdcss
-sudo pacman -S libdvdread
 # cmd to play dvd
 # mpv dvd://
-paru -S google-chrome
-sudo apt install build-essential pkg-config libgmime-3.0-dev libxapian-dev libsqlite3-dev libgpgme-dev libglib2.0-dev
-pacman -S imv
